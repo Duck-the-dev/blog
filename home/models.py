102ff6d3fced3b22c50ad0bf5bbd406ca2930886
Mahmoud <mahmoud.mohamed30120@gmail.com>
2022-05-08 08:21:13 +0200

    add bootstrap and base.html

--- home/models.py
+++ home/models.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=120)
+    title_tag = models.CharField(max_length=120, default='DevBlog')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
 
